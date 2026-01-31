670ae0515bad3c246cd093e52a95536578015bda
rk7sensi-1 <jg2814313@gmail.com>
2026-01-31 11:55:29 -0300

    Refactor service worker event listeners
    Removed the 'activate' event listener and simplified the 'fetch' event listener.

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-self.addEventListener('install', event => {
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener("fetch", () => {});
